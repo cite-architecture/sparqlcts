015fc8876f74648d7d56ff7eb2367f0b75b6a9f4
Neel Smith <dnsmith.neel@gmail.com>
2015-01-30 08:07:13 -0500

    Add a failing test

---
+++ specs/java/citeverbs/CiteVerbsTest.java
@@ -0,0 +1,17 @@
+package citeverbs;
+
+import org.concordion.integration.junit4.ConcordionRunner;
+import org.junit.runner.RunWith;
+import org.concordion.integration.junit3.ConcordionTestCase;
+
+/* Run this class as a JUnit test. */
+
+public class CiteVerbsTest extends ConcordionTestCase {
+
+    // SPARQL endpoint to use for tests:
+    String urn = "";
+    
+    public boolean checkVersion(String versionStr) {
+	return false;
+    }
+}
---
+++ specs/markdown/citeverbs/CiteVerbs.md
@@ -0,0 +1,7 @@
+
+# RDF verbs in `sparqlcts` #
+
+
+This specification applies to version <code concordion:set="#version">0.9.6</code> of the `sparqlcts` implementation of Canonical Text Services.  (The fixture implementing tests of this specification <strong concordion:assertTrue="checkVersion(#version)">must be configured to use this version</strong>.)
+
+
